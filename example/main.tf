3eb25baa77d1bf316e17445d995c9453530317a0
Mathieu Benoit <mabenoit@microsoft.com>
2019-03-16 23:01:03 -0400

    Add rg creation example
    Signed-off-by: Mathieu Benoit <mabenoit@microsoft.com>

---
+++ example/main.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  version = "~>1.23"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
---
+++ example/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+  description = "Name of the resource group."
+}
+
+variable "location" {
+  description = "Location of the resource group."
+  default = "eastus"
+}
