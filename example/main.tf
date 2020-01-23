65c25b9c7fff24cba6bcba8ffd72bad083c34a82
Mathieu Benoit <mabenoit@microsoft.com>
2020-01-23 10:29:01 -0500

    fix interpolation warning + azurerm 1.41.0

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  version = "=1.33.0"
+  version = "=1.41.0"
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
